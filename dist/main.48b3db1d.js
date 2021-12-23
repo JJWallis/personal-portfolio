e1ceb7f4071d299ea7d17bda5b2d21c8069a3ce4
JJWallis <josh.jamesonwallis@gmail.com>
2021-12-23 14:32:36 +0000

    Nav ip

--- dist/main.48b3db1d.js
+++ dist/main.48b3db1d.js
@@ -189,7 +189,7 @@ var reloadCSS = require('_css_loader');
 
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"./../assets/icons/hamburger.svg":[["hamburger.e6d5616a.svg","assets/icons/hamburger.svg"],"assets/icons/hamburger.svg"],"./../assets/icons/close.svg":[["close.c9a8b322.svg","assets/icons/close.svg"],"assets/icons/close.svg"],"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
