d9c9b062703208a4590cc40a80e37153c4520e2c
JJWallis <josh.jamesonwallis@gmail.com>
2022-01-16 12:56:12 +0000

    Hamburger setup + working on homepage

--- src/scripts/nav.ts
+++ src/scripts/nav.ts
@@ -5,9 +5,11 @@ hamburger.addEventListener('click', () => {
    const isSelected = hamburger.getAttribute('aria-selected')
    if (isSelected === 'false') {
       hamburger.setAttribute('aria-selected', 'true')
+      hamburger.classList.add('active')
       nav.setAttribute('data-visible', 'true')
    } else {
       hamburger.setAttribute('aria-selected', 'false')
+      hamburger.classList.remove('active')
       nav.setAttribute('data-visible', 'false')
    }
 })
